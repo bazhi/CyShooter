7c1f427038feba56226a5fda39497c8522bbe171
bazhi <bazhicc@gmail.com>
2021-06-11 15:01:49 +0800

    重命名

---
+++ Source/CyShooter/CyShooter.Build.cs
@@ -0,0 +1,21 @@
+// Project:         Advanced Locomotion System V4 on C++
+// Copyright:       Copyright (C) 2021 Doğa Can Yanıkoğlu
+// License:         MIT License (http://www.opensource.org/licenses/mit-license.php)
+// Source Code:     https://github.com/bazhicc/CyShooter
+// Original Author: Doğa Can Yanıkoğlu
+// Contributors:  
+
+using UnrealBuildTool;
+
+public class CyShooter : ModuleRules
+{
+	public CyShooter(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new[]
+			{"Core", "CoreUObject", "Engine", "InputCore", "NavigationSystem", "AIModule", "GameplayTasks","PhysicsCore", "Niagara"});
+
+		PrivateDependencyModuleNames.AddRange(new[] {"Slate", "SlateCore"});
+	}
+}
